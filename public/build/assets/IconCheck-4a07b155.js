da59c10eceac673cc72f88460f9ab7875318050f
Pedro Montañez <pedromonta549@gmail.com>
2024-06-22 12:24:12 -0400

    Add build

---
+++ public/build/assets/IconCheck-4a07b155.js
@@ -0,0 +1,1 @@
+import{c}from"./createReactComponent-a8208e3e.js";var o=c("check","IconCheck",[["path",{d:"M5 12l5 5l10 -10",key:"svg-0"}]]);export{o as I};
---
+++ public/build/assets/IconFile-68db03ec.js
@@ -0,0 +1,1 @@
+import{c as a}from"./createReactComponent-a8208e3e.js";var o=a("file","IconFile",[["path",{d:"M14 3v4a1 1 0 0 0 1 1h4",key:"svg-0"}],["path",{d:"M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z",key:"svg-1"}]]);export{o as I};
