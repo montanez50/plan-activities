9a54f8ef68e8aa1244d4ad6981dc8b3ebd08192a
Pedro Montañez <pedromonta549@gmail.com>
2024-06-26 08:17:10 -0400

    LV

--- config/log-viewer.php
+++ config/log-viewer.php
@@ -88,7 +88,8 @@ return [
 
     'api_middleware' => [
         \Opcodes\LogViewer\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-        \Opcodes\LogViewer\Http\Middleware\AuthorizeLogViewer::class,
+        // \Opcodes\LogViewer\Http\Middleware\AuthorizeLogViewer::class,
+        'role:administrador'
     ],
 
     'api_stateful_domains' => env('LOG_VIEWER_API_STATEFUL_DOMAINS') ? explode(',', env('LOG_VIEWER_API_STATEFUL_DOMAINS')) : null,
